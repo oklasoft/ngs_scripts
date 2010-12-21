6535ecd576e28846b0177ceb5e87a61da8a07b34
Stuart Glenn <glennsb@lupus.omrf.org>
2010-12-21 10:03:28 -0600

    support for single or paired end

--- bwa_sampese_qsub_tasked.rb
+++ bwa_sampese_qsub_tasked.rb
@@ -4,11 +4,31 @@ output_base = ARGV.shift
 reference = ARGV.shift
 index = (ENV['SGE_TASK_ID']||1).to_i - 1
 
-inputs = ARGV.each_slice(5).to_a[index]
-tag = inputs.shift
-output = File.join(output_base,"#{File.basename(inputs.first,"_1.sai")}.sam")
+groups = [] #ARGV.each_slice(5).to_a[index]
 
-cmd = "bwa sampe -r \"#{tag}\" -f #{output} #{reference} #{inputs.join(" ")}"
+ARGV.each do |a|
+  data << {}
+
+  if "paired" == a then
+    data[:mode] = "sampe"
+  else
+    data[:mode] = "samse"
+  end
+  data[:tag] = ARGV.shift
+  data[:inputs] = ["#{ARGV.shift}"]
+  
+  data[:inputs] << ["#{ARGV.shift}"] if "sampe" == data[:mode]
+  
+  groups << data
+end
+
+data = groups[index]
+
+tag = data[:tag]
+
+output = File.join(output_base,"#{index}.sam")
+
+cmd = "bwa #{data[:mode]} -r \"#{tag}\" -f #{output} #{reference} #{data[:inputs].join(" ")}"
 
 puts cmd
 STDOUT.flush
