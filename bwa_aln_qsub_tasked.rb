6c7773f73666f4761ccd6ee6965371dfae12d939
Stuart Glenn <Stuart-Glenn@omrf.org>
2011-09-26 10:44:18 -0500

    Uses NSLOTS for threaded size

--- bwa_aln_qsub_tasked.rb
+++ bwa_aln_qsub_tasked.rb
@@ -3,12 +3,13 @@
 output_base = ARGV.shift
 reference = ARGV.shift
 index = ENV['SGE_TASK_ID'].to_i - 1
+slots = ENV['NSLOTS']
 
 inputs = ARGV.each_slice(2).to_a[index]
 
 output = File.join(output_base,"#{File.basename(inputs.last,".bam")}-#{inputs.first}.sai")
 
-cmd = "bwa aln -t 12 -f #{output} #{reference} -b#{inputs.first} #{inputs.last}"
+cmd = "bwa aln -t #{slots} -f #{output} #{reference} -b#{inputs.first} #{inputs.last}"
 
 puts cmd
 STDOUT.flush
