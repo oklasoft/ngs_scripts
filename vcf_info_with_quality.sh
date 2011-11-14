438b7fffaa4602c22fddd4624f775e29f871969f
Stuart Glenn <Stuart-Glenn@omrf.org>
2011-11-14 11:44:42 -0600

    Graham made some more changes

--- vcf_info_with_quality.sh
+++ vcf_info_with_quality.sh
@@ -8,8 +8,8 @@ base=`basename ${1} .vcf`
 vcftools --vcf ${1} --get-INFO AB --get-INFO AC --get-INFO AF --get-INFO AN --get-INFO BaseQRankSum --get-INFO DP --get-INFO Dels --get-INFO FS --get-INFO HRun --get-INFO HaplotypeScore --get-INFO InbreedingCoeff --get-INFO MQ --get-INFO MQ0 --get-INFO MQRankSum --get-INFO QD --get-INFO ReadPosRankSum --get-INFO SB --get-INFO VQSLOD --site-quality --out ${PID}
 awk '{print $3}' ${PID}.lqual > temp1.${PID}
 paste ${PID}.INFO temp1.${PID} > temp2.${PID}
-echo "TAG" > temp3.${PID}
-grep -v ^\# ${1} | awk '{print $7}' >> temp3.${PID}
+echo "TAG	dbSNP" > temp3.${PID}
+awk '{OFS="\t"; if (!/^#/){print $7, $3}}' ${1} >> temp3.${PID}
 paste temp2.${PID} temp3.${PID} > temp4.${PID}
 grep -v \? temp4.${PID} > ${base}_all_info.txt
 
