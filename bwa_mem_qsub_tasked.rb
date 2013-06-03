d72e757df66f95ec307ea3bc15af22bf8b499e5d
Stuart Glenn <Stuart-Glenn@omrf.org>
2013-06-03 09:02:54 -0500

    Changes bwa mem to pipe to picard for sort right away

--- bwa_mem_qsub_tasked.rb
+++ bwa_mem_qsub_tasked.rb
@@ -28,11 +28,17 @@ data = groups[index]
 
 tag = data[:tag]
 
-output = File.join(output_base,"#{index}.sam")
+output = File.join(output_base,"#{index}.bam")
 
 cmd = "bwa mem -M -t #{threads} -R \"#{tag}\" #{reference} #{data[:inputs].join(" ")}"
+cmd += "| picard SortSam TMP_DIR=./tmp VALIDATION_STRINGENCY=LENIENT MAX_RECORDS_IN_RAM=3000000 OUTPUT=#{output} SORT_ORDER=coordinate INPUT=/dev/stdin"
 
 puts cmd
 STDOUT.flush
-STDOUT.reopen(File.open(output, 'w'))
-exec cmd
+#STDOUT.reopen(File.open(output, 'w'))
+#exec cmd
+if system cmd
+  exit 0
+else
+  exit $?
+end
