985e79f4b46c02df66abd7f0d8bcc7863a443adb
Stuart Glenn <Stuart-Glenn@omrf.org>
2015-04-23 10:27:51 -0500

    Bump version

--- analyze_sequence_to_snps.rb
+++ analyze_sequence_to_snps.rb
@@ -742,8 +742,8 @@ end
 
 
 class AnalysisTemplaterApp
-VERSION       = "3.2.0"
-REVISION_DATE = "20140717"
+VERSION       = "4.0.0"
+REVISION_DATE = "20150423"
 AUTHOR        = "Stuart Glenn <Stuart-Glenn@omrf.org>"
 COPYRIGHT     = "Copyright (c) 2012-2015 Oklahoma Medical Research Foundation"
 
