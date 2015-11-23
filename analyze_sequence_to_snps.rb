774cab7bb9e0dcf72942609be276d84317f8b0ad
Stuart Glenn <Stuart-Glenn@omrf.org>
2015-11-22 18:26:13 -0600

    Remove loading tabix

--- analyze_sequence_to_snps.rb
+++ analyze_sequence_to_snps.rb
@@ -146,14 +146,12 @@ module unload samtools
 module unload picard
 module unload gatk
 module unload fastqc
-module unload tabix
 module unload btags
 #{aligner_unload_load()}
 module load samtools/1.2
 module load picard/1.118
 module load gatk/3.4-46
 module load fastqc/0.11.1
-module load tabix/0.2.6
 
 set -o pipefail
 EOS
