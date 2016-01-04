d734826e2fcfb5c138b5b58c3291820c03f398a5
Stuart Glenn <Stuart-Glenn@omrf.org>
2016-01-04 17:55:32 -0600

    Update to getting swift type files

--- bwa_mem_qsub_tasked.rb
+++ bwa_mem_qsub_tasked.rb
@@ -1,6 +1,15 @@
 #!/usr/bin/env ruby
 
 require 'tmpdir'
+require 'tempfile'
+require 'uri'
+
+module URI
+  class O3 < Generic
+    USE_REGISTRY = true
+  end
+  @@schemes['O3'] = O3
+end
 
 args = ARGV.clone
 tmp_base = args.shift
@@ -29,9 +38,46 @@ data = groups[index]
 tag = data[:tag]
 output = File.join(output_base,"#{index}.bam")
 
+delete_files = []
+data[:inputs].map! do |i|
+  if i =~ /^(https?|o3):\/\//
+    u = URI(i)
+    base = File.basename(u.path)
+    f = Tempfile.new(base)
+    f.close
+    tmp_file = f.path + File.extname(base)
+    f.unlink
+    delete_files << tmp_file
+    cmd = case u.scheme
+    when /^https?/
+      # curl it
+      %W/curl --retry 3 -f -o #{tmp_file} #{i}/
+    when /^o3/
+      # swift it
+      (container,object)= u.path.scan(/^\/([^\/]+)\/(.*)/)[0]
+      %W/swift download -R 3 -o #{tmp_file} #{container} #{object}/
+    end
+    pid = spawn(*cmd,STDOUT=>STDERR)
+    pid, status = Process.wait2(pid)
+    if nil == status
+      raise "Unable to start object download"
+    elsif 0 != status.exitstatus
+      raise "Failure downloading object: #{$?}"
+    end
+    tmp_file
+  else
+    i
+  end
+end
+
 pre = ""
 post = ""
 data[:inputs].map! do |i|
+  #if i =~ /^http.*/ && i =~ /\.xz$/
+    #pre ="(t=`mktemp`;"
+    #post = " && rm ${t})"
+    #(account,cont,obj)=i.scan(/^https:\/\/o3.omrf.org\/v1\/([^\/]+)\/([^\/]+)\/(.*)/)[0]
+    #"<(swift download -R 3 #{cont} #{obj} ; xzcat #{obj}  || rm -f ${t})"
   if i =~ /\.xz$/
     pre ="(t=`mktemp`;"
     post = " && rm ${t})"
