553f6c4eb2932eba5c6a66cad1ce34e3427fa606
Stuart Glenn <Stuart-Glenn@omrf.org>
2016-04-11 10:54:36 -0500

    Increase http read timeout

--- alignment_report.rb
+++ alignment_report.rb
@@ -8,6 +8,7 @@
 # - Percent undetermined for the demuxed flowcell (bcl2fastq)
 # <=10
 # - Average %Q30 across all lanes for each sample (bcl2fastq)
+sleep .5
 # =75
 # - Average mean Q score across all lanes for each sample (bcl2fastq)
 # =30
@@ -137,7 +138,7 @@ def demux_stats_from_o3(object_path)
   http.use_ssl=true
   http.verify_mode = OpenSSL::SSL::VERIFY_PEER
   http.open_timeout = 2
-  http.read_timeout = 2
+  http.read_timeout = 15
   req  = Net::HTTP::Head.new(u.request_uri)
   req["X-Auth-Token"] = token
   resp = http.request(req)
