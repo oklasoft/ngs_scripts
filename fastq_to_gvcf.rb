e70f3ab95d0d59b04c7080270dadf698569db133
Stuart Glenn <Stuart-Glenn@omrf.org>
2016-04-15 14:10:33 -0500

    Version bump

--- fastq_to_gvcf.rb
+++ fastq_to_gvcf.rb
@@ -699,8 +699,8 @@ end
 
 
 class AnalysisTemplaterApp
-VERSION       = "4.3.0"
-REVISION_DATE = "20160408"
+VERSION       = "4.3.1"
+REVISION_DATE = "20160415"
 AUTHOR        = "Stuart Glenn <Stuart-Glenn@omrf.org>"
 COPYRIGHT     = "Copyright (c) 2012-2016 Oklahoma Medical Research Foundation"
 
