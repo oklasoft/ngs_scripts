73e93d0d8e73ddeed32f1097a9e35124b4858400
Stuart Glenn <Stuart-Glenn@omrf.org>
2018-01-11 18:38:20 +0000

    Bump version on non clia mainline branch

--- fastq_to_gvcf.rb
+++ fastq_to_gvcf.rb
@@ -710,8 +710,8 @@ end
 
 
 class AnalysisTemplaterApp
-VERSION       = "4.6.0-clia"
-REVISION_DATE = "20171029"
+VERSION       = "4.6.0"
+REVISION_DATE = "20180111"
 AUTHOR        = "Stuart Glenn <Stuart-Glenn@omrf.org>"
 COPYRIGHT     = "Copyright (c) 2012-2017 Oklahoma Medical Research Foundation"
 
